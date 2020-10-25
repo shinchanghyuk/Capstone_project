eef1356078911de1140649a6a5d6d933d9aec8ca
isactually <isac1945@naver.com>
2020-10-25 18:05:19 +0900

    php로 db연동

--- app/src/main/java/com/example/capstone_project/StadiumDetailsActivity.java
+++ app/src/main/java/com/example/capstone_project/StadiumDetailsActivity.java
@@ -1,34 +1,168 @@
 package com.example.capstone_project;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class StadiumDetailsActivity extends AppCompatActivity {
+
+    String myJSON;
+
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ADD = "address";
+    private static final String TAG_TIME = "time";
+    private static final String TAG_CHARGE = "charge";
+    private static final String TAG_TEL = "tel";
+
+    JSONArray st = null;
+
+    ArrayList<HashMap<String, String>> stadiumList;
+
+    ListView list;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stadium_details);
 
+        list = (ListView) findViewById(R.id.listView);
+        stadiumList = new ArrayList<HashMap<String, String>>();
+        getData("http://192.168.219.108/PHP_connection.php"); //ip주소 자기 ip주소로 바꿔서 넣어야 됨
+    }
+
+    protected void showList() {
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            st = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for (int i = 0; i < st.length(); i++) {
+                JSONObject c = st.getJSONObject(i);
+                String name = c.getString(TAG_NAME);
+                String address = c.getString(TAG_ADD);
+                String time = c.getString(TAG_TIME);
+                String charge = c.getString(TAG_CHARGE);
+                String tel = c.getString(TAG_TEL);
+
+                HashMap<String, String> sts = new HashMap<String, String>();
+
+                sts.put(TAG_NAME, name);
+                sts.put(TAG_ADD, address);
+                sts.put(TAG_TIME, time);
+                sts.put(TAG_CHARGE, charge);
+                sts.put(TAG_TEL, tel);
+
+                stadiumList.add(sts);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    StadiumDetailsActivity.this, stadiumList, R.layout.list_item,
+                    new String[]{TAG_NAME, TAG_ADD, TAG_TIME, TAG_CHARGE, TAG_TEL},
+                    new int[]{R.id.name, R.id.address, R.id.time, R.id.charge, R.id.tel}
+            );
+
+            list.setAdapter(adapter);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getData(String url) {
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return null;
+                }
+
+
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                myJSON = result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
+    }
+}
+
+        /*
         TextView stadium_address = (TextView) findViewById(R.id.stadium_address);
         TextView stadium_time = (TextView) findViewById(R.id.stadium_time);
         TextView stadium_special = (TextView) findViewById(R.id.stadium_special);
         TextView stadium_charge = (TextView) findViewById(R.id.stadium_charge);
         TextView stadium_tele = (TextView) findViewById(R.id.stadium_tele);
 
-        TextView stadium_nameTv = findViewById(R.id.stadium_nameTv);
-        ImageView stadium_image = (ImageView) findViewById(R.id.stadium_imageView);
+         */
 
-        Intent intent = getIntent();
-        String stadium = intent.getExtras().getString("stadium");
-        String stadiumName = intent.getExtras().getString("stadiumname");
 
-        stadium_nameTv.setText(stadiumName);
 
+
+
+
+/*
+    TextView stadium_nameTv = findViewById(R.id.stadium_nameTv);
+    ImageView stadium_image = (ImageView) findViewById(R.id.stadium_imageView);
+
+    Intent intent = getIntent();
+    String stadium = intent.getExtras().getString("stadium");
+    String stadiumName = intent.getExtras().getString("stadiumname");
+
+
+ */
+
+
+
+
+
+
+
+
+        //stadium_nameTv.setText(stadiumName);
+/*
         if(stadium.equals("m0")){
             //월드컵
             stadium_address.setText("서울특별시 마포구 성산2동 월드컵로 240");
@@ -70,5 +204,5 @@ public class StadiumDetailsActivity extends AppCompatActivity {
             stadium_tele.setText("031-8045-2413");
             stadium_image.setImageResource(R.drawable.e);
         }
-    }
-}
+
+ */
--- app/src/main/java/com/example/capstone_project/StadiumSelectActivity.java
+++ app/src/main/java/com/example/capstone_project/StadiumSelectActivity.java
@@ -1,11 +1,11 @@
 package com.example.capstone_project;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,16 +21,8 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 
-import noman.googleplaces.NRPlaces;
-import noman.googleplaces.Place;
-import noman.googleplaces.PlaceType;
-import noman.googleplaces.PlacesException;
-import noman.googleplaces.PlacesListener;
-
-public class StadiumSelectActivity extends AppCompatActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback, PlacesListener{
+public class StadiumSelectActivity extends AppCompatActivity implements OnMapReadyCallback {
     MapFragment mapFrag;
     GoogleMap gMap;
     ListView list;
@@ -38,23 +30,10 @@ public class StadiumSelectActivity extends AppCompatActivity implements OnMapRea
     GroundOverlayOptions videoMark;
     String stadium;
 
-    List<Marker> previous_marker = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stadium_select);
-
-        previous_marker = new ArrayList<Marker>();
-
-        Button stadium_search = (Button)findViewById(R.id.stadium_search);
-
-        stadium_search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showPlaceInformation(new LatLng(37.568256, 126.897240));
-            }
-        });
 
 
         stadium_Data.add("월드컵 경기장");
@@ -97,71 +76,6 @@ public class StadiumSelectActivity extends AppCompatActivity implements OnMapRea
             }
         });
     }
-
-    @Override
-    public void onPlacesFailure(PlacesException e) {
-
-    }
-
-    @Override
-    public void onPlacesStart() {
-
-    }
-
-    @Override
-    public void onPlacesSuccess(final List<Place> places) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                for (noman.googleplaces.Place place : places) {
-
-                    LatLng latLng
-                            = new LatLng(place.getLatitude()
-                            , place.getLongitude());
-
-                    //String markerSnippet = getCurrentAddress(latLng); //현재위치
-
-                    MarkerOptions markerOptions = new MarkerOptions();
-                    markerOptions.position(latLng);
-                    markerOptions.title(place.getName());
-                    markerOptions.snippet("테스트");
-                    Marker item = gMap.addMarker(markerOptions);
-                    previous_marker.add(item);
-
-                }
-
-                //중복 마커 제거
-                HashSet<Marker> hashSet = new HashSet<Marker>();
-                hashSet.addAll(previous_marker);
-                previous_marker.clear();
-                previous_marker.addAll(hashSet);
-
-            }
-        });
-    }
-
-    @Override
-    public void onPlacesFinished() {
-
-    }
-
-    public void showPlaceInformation(LatLng location)
-    {
-        gMap.clear();//지도 클리어
-
-        if (previous_marker != null)
-            previous_marker.clear();//지역정보 마커 클리어
-
-        new NRPlaces.Builder()
-                .listener(StadiumSelectActivity.this)
-                .key("AIzaSyCYcO3aYjvpwhVMI-5RdW3de9xcThjSoeI")
-                .latlng(37.568256, 126.897240)//위치
-                .radius(2000) //2000 미터 내에서 검색
-                .type(PlaceType.STADIUM) //구장
-                .build()
-                .execute();
-    }
-
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
